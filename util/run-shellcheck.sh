c8b71419891a58bd4f268eb5a207dd906badbd02
Andreas Langenhagen <andreas@langenhagen.cc>
2019-08-15 13:59:45 +0200

    Refactor run-shellcheck.sh

--- util/run-shellcheck.sh
+++ util/run-shellcheck.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
-# Runs the shellcheck linter with the project's preferences.
+# Runs the linter shellcheck with the project's preferences.
 # Run it from the project's root directory in order to make shellcheck follow source statements.
 #
 # author: andreasl
 
-# ignore
+cd src || exit 1
+# ignore the error codes SC2059 and SC2181
 # SC2059: Don't use variables in the printf format string. Use printf "..%s.." "$foo".
 # SC2181: Check exit code directly with e.g. 'if mycmd;', not indirectly with $?.
-cd src || exit 1
 shellcheck -x --exclude SC2059,SC2181 *
