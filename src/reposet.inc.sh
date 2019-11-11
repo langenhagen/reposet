c7f88e771612327069642aa678ab00a32bbafe5f
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2019-11-11 16:06:36 +0100

    Change comment

--- src/reposet.inc.sh
+++ src/reposet.inc.sh
@@ -106,7 +106,7 @@ function git_push_or_die {
     fi
     code="$?"
     if [ "$code" -eq 1 ] ; then
-        # 1 error from gerrit side
+        # possibly rejected from gerrit side
         msg="Calling \`${yb}git push ${push_remote} ${local_branch}:${push_branch}${y}\`"
         msg+=" on the repo ${yb}${repo_path}${y} failed with exit code 1."
         >&2 printf -- "${y}$msg${n}\n"
