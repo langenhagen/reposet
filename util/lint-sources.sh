8d91417d8434230b17afef8b437a6b71f8d006f5
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2022-03-18 12:03:58 +0100

    Improve robustness of lint-sources.sh
    Also disable a shellcheck warning when linting `lint-sources.sh`.

--- util/lint-sources.sh
+++ util/lint-sources.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-# Runs the linter shellcheck with the project's preferences.
+# Run the linter shellcheck with the project's preferences.
 # Run it from the project's root directory in order to make shellcheck follow source statements.
 #
 # author: andreasl
 
-cd src || exit 1
-# ignore the error codes SC2059 and SC2181
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "${script_dir}/../src" || exit 1
+
+# ignore the following error codes:
 # SC2059: Don't use variables in the printf format string. Use printf "..%s.." "$foo".
 # SC2181: Check exit code directly with e.g. 'if mycmd;', not indirectly with $?.
+# shellcheck disable=SC2035
 shellcheck -x --exclude SC2059,SC2181 *
